5a3c8f4d00842fd18464107027ecc656cfed4ec5
ugnius <ugnius.vilkoncius@gmail.com>
2024-02-03 20:25:49 +0200

    React init

---
+++ DocManagerUI/src/App.tsx
@@ -0,0 +1,11 @@
+import { FormControl, Input } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <FormControl>
+      <Input placeholder="Test"></Input>
+    </FormControl>
+  );
+}
+
+export default App;
