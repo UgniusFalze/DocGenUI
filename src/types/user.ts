4704f4041aad891a9f85e90f44687c951a2fb511
ugnius <ugnius.vilkoncius@gmail.com>
2024-07-15 20:55:53 +0300

    Vat code form input

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,18 +1,20 @@
 export type UserForm = {
-    address: string;
-    personalId: string;
-    freelanceWorkId: string;
-    bankNumber: string;
-    bankName: string;
-}
+  address: string;
+  personalId: string;
+  freelanceWorkId: string;
+  bankNumber: string;
+  bankName: string;
+  vatCode: string | null;
+};
 
 export type UserProfile = {
-    userId: string;
-    firstName: string;
-    lastName: string;
-    address: string;
-    personalId: string;
-    freelanceWorkId: string;
-    bankNumber: string;
-    bankName: string;
-}
+  userId: string;
+  firstName: string;
+  lastName: string;
+  address: string;
+  personalId: string;
+  freelanceWorkId: string;
+  bankNumber: string;
+  bankName: string;
+  vatCode: string | null;
+};
